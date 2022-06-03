5bc803558e6625c94bfe0032bbd8bfe7903a17d8
kwy <scoworks.kwy@gmail.com>
2022-06-03 22:21:06 +0900

    myRestaurant

--- src/restaurants/dtos/edit-restaurant.ts
+++ src/restaurants/dtos/edit-restaurant.ts
@@ -1,10 +1,4 @@
-import {
-  Field,
-  InputType,
-  ObjectType,
-  PartialType,
-  PickType,
-} from '@nestjs/graphql';
+import { Field, InputType, ObjectType, PartialType } from '@nestjs/graphql';
 import { CoreOutput } from 'src/common/dtos/output.dto';
 import { CreateRestaurantInput } from './create-restaurant.dto';
 
